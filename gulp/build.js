835db7bc5a9f56a4b0ef8cbf176ea2ac8bbe9ef3
Peter Orum <peter@peterorum.com>
2015-07-31 15:46:54 +1000

    deployed v1

--- gulp/build.js
+++ gulp/build.js
@@ -81,7 +81,7 @@ gulp.task('other', function () {
 
   return gulp.src([
     path.join(conf.paths.src, '/**/*'),
-    path.join('!' + conf.paths.src, '/**/*.{html,css,js,scss}')
+    path.join('!' + conf.paths.src, '/**/*.{html,css,js,scss,sublime-workspace,sublime-project,psd}')
   ])
     .pipe(fileFilter)
     .pipe(gulp.dest(path.join(conf.paths.dist, '/')));
