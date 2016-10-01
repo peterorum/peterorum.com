2f9518774ffc913e1fe61b5c6e32af0229d047b8
Peter Orum <peter@peterorum.com>
2016-10-01 17:21:11 +1000

    multiple background images

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -2,6 +2,25 @@ class MainController {
   constructor() {
     'ngInject';
 
+    let background = 0;
+    let count = 6;
+
+    // preload
+
+    for (let i = 0; i < count; i++){
+      let div = $(`<div class='hidden pjo-photo-${i}'></div>`);
+      $('body').append(div);
+    }
+
+    setInterval(function() {
+
+      $('.pjo-photographer').removeClass(`pjo-photo-${background}`);
+
+      background = (background + 1) % count;
+
+      $('.pjo-photographer').addClass(`pjo-photo-${background}`);
+
+    }, 5000);
   }
 }
 
